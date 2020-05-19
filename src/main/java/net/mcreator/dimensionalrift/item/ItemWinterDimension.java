fb57d2d57fede01f52a216271c5a0587a348dbe9
rclca <rclca@10.0.0.43>
2020-05-19 12:44:02 -0700

    First try at Winter Dimension

---
+++ src/main/java/net/mcreator/dimensionalrift/item/ItemWinterDimension.java
@@ -0,0 +1,42 @@
+
+package net.mcreator.dimensionalrift.item;
+
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+import net.minecraft.world.World;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.creativetab.CreativeTabs;
+
+import net.mcreator.dimensionalrift.world.WorldWinterDimension;
+
+public class ItemWinterDimension extends Item {
+	@GameRegistry.ObjectHolder("dimensionalrift:winterdimension")
+	public static final Item block = null;
+	public ItemWinterDimension() {
+		super();
+		this.maxStackSize = 1;
+		setMaxDamage(64);
+		setCreativeTab(CreativeTabs.TOOLS);
+	}
+
+	@Override
+	public EnumActionResult onItemUse(EntityPlayer entity, World world, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY,
+			float hitZ) {
+		pos = pos.offset(facing);
+		ItemStack itemstack = entity.getHeldItem(hand);
+		if (!entity.canPlayerEdit(pos, facing, itemstack)) {
+			return EnumActionResult.FAIL;
+		} else {
+			if (world.isAirBlock(pos))
+				WorldWinterDimension.portal.portalSpawn(world, pos);
+			itemstack.damageItem(1, entity);
+			return EnumActionResult.SUCCESS;
+		}
+	}
+}
